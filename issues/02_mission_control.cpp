a98c0cec855f7dd1418a4bea97c89ff25b0cddb9
Eddie <bmichellepk@gmail.com>
2026-06-19 08:44:00 +0700

    Add CMake configuration for optional main executable and implement Mission Control exercise

---
+++ issues/02_mission_control.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <iomanip>
+
+int main() {
+    std::cout << "Welcome to Mission Control!" << std::endl;
+    std::cout << "Please enter the astronaut name: ";
+    std::string astronautName;
+    std::cin >> astronautName;
+    std::cout << "Please enter the astronaut age: ";
+    int astronautAge;
+    while (!(std::cin >> astronautAge && astronautAge >= 8 && astronautAge <= 18)) {
+        if (std::cin.fail()) {
+            std::cout << "Invalid input for age\n";
+        } else {
+            std::cout << "Age must be between 8 and 18\n";
+        }
+        std::cin.clear();
+        std::cin.ignore(10000, '\n');
+        return 1;
+    }
+    std::cout << "Please enter the astronaut height in m: ";
+    double astronautHeight;
+    while (!(std::cin >> astronautHeight && astronautHeight >= 1.0 && astronautHeight <= 2.2)) {
+        if (std::cin.fail()) {
+            std::cout << "Invalid input for height\n";
+        } else {
+            std::cout << "Height must be between 1.0 and 2.2 meters\n";
+        }
+        std::cin.clear();
+        std::cin.ignore(10000, '\n');
+        return 1;
+    }
+    std::cout << "Please enter the astronaut weight in kg: ";
+    float astronautWeight;
+    while (!(std::cin >> astronautWeight && astronautWeight >= 20.0 && astronautWeight <= 100.0)) {
+        if (std::cin.fail()) {
+            std::cout << "Invalid input for weight\n";
+        } else {
+            std::cout << "Weight must be between 20.0 and 100.0 kg\n";
+        }
+        std::cin.clear();
+        std::cin.ignore(10000, '\n');
+        return 1;
+    }
+    double astronautBMI = astronautWeight / (astronautHeight * astronautHeight);
+    std::cout << "Please enter the astronaut mission distance in km: ";
+    double missionDistance;
+    while (!(std::cin >> missionDistance && missionDistance > 0)) {
+        if (std::cin.fail()) {
+            std::cout << "Invalid input for mission distance\n";
+        } else {
+            std::cout << "Mission distance must be bigger than 0\n";
+        }
+        std::cin.clear();
+        std::cin.ignore(10000, '\n');
+        return 1;
+    }
+    std::cout << "Please enter the astronaut ship speed in km/h: ";
+    float shipSpeed;
+    while (!(std::cin >> shipSpeed && shipSpeed > 0)) {
+        if (std::cin.fail()) {
+            std::cout << "Invalid input for ship speed\n";
+        } else {
+            std::cout << "Ship speed must be a bigger than 0\n";
+        }
+        std::cin.clear();
+        std::cin.ignore(10000, '\n');
+        return 1;
+    }
+    std::cout << "Please enter the astronaut has helmet? (1 for yes, 0 for no): ";
+    bool hasHelmet;
+    while (!(std::cin >> hasHelmet)) {
+        std::cout << "Invalid input for helmet status\n";
+        std::cin.clear();
+        std::cin.ignore(10000, '\n');
+        return 1;
+    }
+
+    std::cout << "=================Mission Control=================" << std::endl;
+    std::cout << "Astronaut Name: " << astronautName << std::endl;
+    std::cout << "Astronaut Age: " << astronautAge << std::endl;
+    std::cout << "Astronaut Height: " << astronautHeight << " m" << std::endl;
+    std::cout << "Astronaut Weight: " << astronautWeight << " kg" << std::endl;
+    std::cout << "Mission Distance: " << missionDistance << " km" << std::endl;
+    std::cout << "Ship Speed: " << shipSpeed << " km/h" << std::endl;
+    std::cout << "Astronaut has helmet: " << (hasHelmet ? "Yes" : "No") << std::endl;
+
+    std::cout << "=================Astronaut Report=================" << std::endl;
+    std::cout << "Astronaut Name: " << astronautName << std::endl;
+    std::cout << "Astronaut Age: " << astronautAge << std::endl;
+    std::cout << "Astronaut Height: " << astronautHeight << " m" << std::endl;
+    std::cout << "Earth weights the astronaut at " << astronautWeight << " kg" << std::endl;
+    std::cout << "Moon weights the astronaut at " << astronautWeight * 0.165 << " kg" << std::endl;
+    std::cout << "Mars weights the astronaut at " << astronautWeight * 0.38 << " kg" << std::endl;
+    std::cout << "Astronaut BMI: " << astronautBMI << std::endl;
+
+    std::cout << "==================Mission Time==================" << std::endl;
+    std::cout << "Distance default format: " << missionDistance << " km" << std::endl;
+    std::cout << "Distance fixed format: " << std::fixed << std::setprecision(2) << missionDistance << " km" << std::endl;
+    std::cout << "Distance scientific format: " << std::scientific << std::setprecision(6) << missionDistance << " km" << std::endl;
+    std::cout << "Estimated mission time: " << int(missionDistance / shipSpeed) * 3600 << " seconds" << std::endl;
+    std::cout << "Estimated mission time: " << int(missionDistance / shipSpeed) << " hours" << std::endl;
+    std::cout << "Mission status: " << (hasHelmet ? "READY" : "NOT READY") << std::endl;
+
+    return 0;
+}
